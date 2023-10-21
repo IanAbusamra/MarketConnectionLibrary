ce1636085eea2705c20522a344cd5278bd01fb8a
IanAbusamra <76963772+IanAbusamra@users.noreply.github.com>
2023-10-21 16:01:38 -0400

    setup

---
+++ Project/target/debug/Project.d
@@ -0,0 +1,1 @@
+C:\Users\Owner\Desktop\MarketConnectionLibrary\Project\target\debug\Project.exe: C:\Users\Owner\Desktop\MarketConnectionLibrary\Project\src\main.rs
---
+++ Project/target/debug/deps/Project.d
@@ -0,0 +1,5 @@
+C:\Users\Owner\Desktop\MarketConnectionLibrary\Project\target\debug\deps\Project.exe: src\main.rs
+
+C:\Users\Owner\Desktop\MarketConnectionLibrary\Project\target\debug\deps\Project.d: src\main.rs
+
+src\main.rs:
