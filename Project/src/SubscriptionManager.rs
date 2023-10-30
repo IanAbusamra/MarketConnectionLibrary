69888e3860d0d6ef526d4fd5a339fabbe855946c
kevins19 <kevin.j.shan@gmail.com>
2023-10-30 18:53:41 -0400

    added update_subscriptions

--- Project/src/SubscriptionManager.rs
+++ Project/src/SubscriptionManager.rs
@@ -10,16 +10,20 @@ impl SubscriptionManager{
         while s.exchange_listener.id >= self.lookup.len() {
             self.lookup.push(HashMap::new());
         }
+
         // no clue if the below will work or not
         *(self.lookup[s.exchange_lister.id].entry(s.attribute).or_insert(Vec::new())).push(s);
     }
     pub fn get_subscriptions(&self, e: &impl ExchangeListener, a: &String) -> List<Subscription> {
         self.lookup[e.id][a]
     }
-    pub fn update_subscriptions(&self, e: &impl ExchangelLstener, a: &String) {
+    pub fn update_subscriptions<T:DataPacket>(&self, e: &impl ExchangelListener, a: &String, val : T) {
         let target_subscriptions = self.get_subscriptions(e, a);
-        
-        // loop through target_subscriptions and update 
-        todo!();
+
+        // iterate through subscriptions and update according to val
+        // details will need to be worked out with other teams
+        for sub in target_subscriptions.iter() {
+            *sub.data_structure.update(val);        
+        }
     }
 }
