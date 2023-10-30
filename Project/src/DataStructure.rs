43f2ad94b10ca6b679020e4fdb867999ad32a0e0
kevins19 <kevin.j.shan@gmail.com>
2023-10-30 18:54:27 -0400

    cha

--- Project/src/DataStructure.rs
+++ Project/src/DataStructure.rs
@@ -1,37 +1,27 @@
 use crate::DataPacket;
 
-pub struct Subscription {
-    
+pub struct DataStructure {
+    size: i32,
+    storage: Vec<DataPacket>,
 }
 
-impl Subscription{
-    fn update() {
-        
+impl DataStructure {
+    pub fn new() -> Self {
+        DataStructure {
+            size: 0,
+            storage: Vec::new(),
+        }
+    }
+
+    pub fn add_datapacket(dp: DataPacket) {
+        vec.push(dp);
+        size = size + 1;
+    }
+
+    pub fn print_ds() {
+        for dp in &ds {
+            //not finished
+            print!("{} ", dp);
+        }
     }
 }
-
-// pub struct DataStructure {
-//     size: i32,
-//     storage: Vec<DataPacket>,
-// }
-
-// impl DataStructure {
-//     pub fn new() -> Self {
-//         DataStructure {
-//             size: 0,
-//             storage: Vec::new(),
-//         }
-//     }
-
-//     pub fn add_datapacket(dp: DataPacket) {
-//         vec.push(dp);
-//         size = size + 1;
-//     }
-
-//     pub fn print_ds() {
-//         for dp in &ds {
-//             //not finished
-//             print!("{} ", dp);
-//         }
-//     }
-// }
