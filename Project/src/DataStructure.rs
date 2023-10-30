299ed7b7eb6c60b080ad31a72f1f3de4b419b34e
IanAbusamra <76963772+IanAbusamra@users.noreply.github.com>
2023-10-30 18:46:53 -0400

    added commented data structure code

--- Project/src/DataStructure.rs
+++ Project/src/DataStructure.rs
@@ -9,3 +9,29 @@ impl Subscription{
         
     }
 }
+
+// pub struct DataStructure {
+//     size: i32,
+//     storage: Vec<DataPacket>,
+// }
+
+// impl DataStructure {
+//     pub fn new() -> Self {
+//         DataStructure {
+//             size: 0,
+//             storage: Vec::new(),
+//         }
+//     }
+
+//     pub fn add_datapacket(dp: DataPacket) {
+//         vec.push(dp);
+//         size = size + 1;
+//     }
+
+//     pub fn print_ds() {
+//         for dp in &ds {
+//             //not finished
+//             print!("{} ", dp);
+//         }
+//     }
+// }
