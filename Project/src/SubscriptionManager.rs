9836bd84c47ed8791badf3a155208d634942e8ac
kevins19 <kevin.j.shan@gmail.com>
2023-10-30 20:42:37 -0400

    added boxing

--- Project/src/SubscriptionManager.rs
+++ Project/src/SubscriptionManager.rs
@@ -1,12 +1,12 @@
 pub struct SubscriptionManager {
-    lookup : Vec<HashMap<String, Vec<Subscription>>>,
+    lookup : Vec<HashMap<String, Vec<Box<dyn Subscription>>>>,
 }
 
 impl SubscriptionManager{
     pub fn new() -> SubscriptionManager {
         Self {Vec::new()}
     }
-    pub fn add_subscription(&self, s: &Subscription) {
+    pub fn add_subscription(&self, s: &Box<dyn Subscription>) {
         while s.exchange_listener.id >= self.lookup.len() {
             self.lookup.push(HashMap::new());
         }
@@ -14,15 +14,10 @@ impl SubscriptionManager{
         // no clue if the below will work or not
         *(self.lookup[s.exchange_lister.id].entry(s.attribute).or_insert(Vec::new())).push(s);
     }
-    pub fn get_subscriptions(&self, e: &impl ExchangeListener, a: &String) -> List<Subscription> {
-        self.lookup[e.id][a]
-    }
-    pub fn update_subscriptions<T:DataPacket>(&self, e: &impl ExchangelListener, a: &String, val : T) {
-        let target_subscriptions = self.get_subscriptions(e, a);
-
+    pub fn update_subscriptions(&self, e: &impl ExchangelListener, a: &String, val : &impl DataPacket) {
         // iterate through subscriptions and update according to val
         // details will need to be worked out with other teams
-        for sub in target_subscriptions.iter() {
+        for sub in self.lookup[e.id][a].iter() {
             *sub.data_structure.add_datapacket(val);        
         }
     }
