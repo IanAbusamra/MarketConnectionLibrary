cddea6eb9a19bc99f03a6456bdf480301633af4b
IanAbusamra <76963772+IanAbusamra@users.noreply.github.com>
2023-11-07 01:22:02 -0500

    added huobi testing

---
+++ TestingArea/huobiconnection/target/debug/build/native-tls-e37480d1d46a7538/build_script_build-e37480d1d46a7538.d
@@ -0,0 +1,5 @@
+C:\Users\Owner\Desktop\MarketConnectionLibrary\TestingArea\huobiconnection\target\debug\build\native-tls-e37480d1d46a7538\build_script_build-e37480d1d46a7538.exe: C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\build.rs
+
+C:\Users\Owner\Desktop\MarketConnectionLibrary\TestingArea\huobiconnection\target\debug\build\native-tls-e37480d1d46a7538\build_script_build-e37480d1d46a7538.d: C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\build.rs
+
+C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\build.rs:
---
+++ TestingArea/huobiconnection/target/debug/deps/cpufeatures-e94f86c09139b9ef.d
@@ -0,0 +1,8 @@
+C:\Users\Owner\Desktop\MarketConnectionLibrary\TestingArea\huobiconnection\target\debug\deps\cpufeatures-e94f86c09139b9ef.rmeta: C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cpufeatures-0.2.11\src\lib.rs C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cpufeatures-0.2.11\src\x86.rs
+
+C:\Users\Owner\Desktop\MarketConnectionLibrary\TestingArea\huobiconnection\target\debug\deps\libcpufeatures-e94f86c09139b9ef.rlib: C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cpufeatures-0.2.11\src\lib.rs C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cpufeatures-0.2.11\src\x86.rs
+
+C:\Users\Owner\Desktop\MarketConnectionLibrary\TestingArea\huobiconnection\target\debug\deps\cpufeatures-e94f86c09139b9ef.d: C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cpufeatures-0.2.11\src\lib.rs C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cpufeatures-0.2.11\src\x86.rs
+
+C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cpufeatures-0.2.11\src\lib.rs:
+C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cpufeatures-0.2.11\src\x86.rs:
