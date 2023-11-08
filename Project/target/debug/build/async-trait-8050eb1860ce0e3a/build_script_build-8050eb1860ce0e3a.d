10c0223154cc7ee86e9b7d12d75a171579f6e70f
IanAbusamra <76963772+IanAbusamra@users.noreply.github.com>
2023-11-07 19:34:32 -0500

    printing data

---
+++ Project/target/debug/build/async-trait-8050eb1860ce0e3a/build_script_build-8050eb1860ce0e3a.d
@@ -0,0 +1,5 @@
+C:\Users\Owner\Desktop\MarketConnectionLibrary\Project\target\debug\build\async-trait-8050eb1860ce0e3a\build_script_build-8050eb1860ce0e3a.exe: C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\async-trait-0.1.74\build.rs
+
+C:\Users\Owner\Desktop\MarketConnectionLibrary\Project\target\debug\build\async-trait-8050eb1860ce0e3a\build_script_build-8050eb1860ce0e3a.d: C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\async-trait-0.1.74\build.rs
+
+C:\Users\Owner\.cargo\registry\src\index.crates.io-6f17d22bba15001f\async-trait-0.1.74\build.rs:
