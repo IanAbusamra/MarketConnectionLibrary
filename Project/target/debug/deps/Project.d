577f077fe6ad1d836406b446e533047ef48ed8e0
IanAbusamra <76963772+IanAbusamra@users.noreply.github.com>
2023-11-19 20:24:16 -0500

    change name again

--- Project/target/debug/deps/Project.d
+++ Project/target/debug/deps/Project.d
@@ -1,6 +1,6 @@
-C:\Users\aniru\OneDrive\Documents\Georgia_Tech\Trading_Club\Fall_2023\MarketConnectionLibrary\Project\target\debug\deps\project.exe: src\main.rs src\binance_exchange_listener.rs src\data_packet.rs src\web_socket.rs src\exchange_listener.rs
+C:\Users\Owner\Desktop\MarketConnectionLibrary\project\target\debug\deps\project.exe: src\main.rs src\binance_exchange_listener.rs src\data_packet.rs src\web_socket.rs src\exchange_listener.rs
 
-C:\Users\aniru\OneDrive\Documents\Georgia_Tech\Trading_Club\Fall_2023\MarketConnectionLibrary\Project\target\debug\deps\project.d: src\main.rs src\binance_exchange_listener.rs src\data_packet.rs src\web_socket.rs src\exchange_listener.rs
+C:\Users\Owner\Desktop\MarketConnectionLibrary\project\target\debug\deps\project.d: src\main.rs src\binance_exchange_listener.rs src\data_packet.rs src\web_socket.rs src\exchange_listener.rs
 
 src\main.rs:
 src\binance_exchange_listener.rs:
---
+++ Project/target/debug/deps/market_connection_library.d
@@ -0,0 +1,9 @@
+C:\Users\Owner\Desktop\MarketConnectionLibrary\project\target\debug\deps\market_connection_library.exe: src\main.rs src\binance_exchange_listener.rs src\data_packet.rs src\web_socket.rs src\exchange_listener.rs
+
+C:\Users\Owner\Desktop\MarketConnectionLibrary\project\target\debug\deps\market_connection_library.d: src\main.rs src\binance_exchange_listener.rs src\data_packet.rs src\web_socket.rs src\exchange_listener.rs
+
+src\main.rs:
+src\binance_exchange_listener.rs:
+src\data_packet.rs:
+src\web_socket.rs:
+src\exchange_listener.rs:
