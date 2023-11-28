e2bc7b955b7b472e75b8a57dcf5e531838d25085
Arpan Banerjee <arnbnrch@gmail.com>
2023-11-28 11:49:28 -0500

    added huobi

--- Project/src/huobi_exchange_listener.rs
+++ Project/src/huobi_exchange_listener.rs
@@ -1,11 +1,20 @@
 use async_trait::async_trait;
 use crate::exchange_listener::ExchangeListener;
-//use crate::market_data::MarketData;
 use crate::web_socket::WebSocket;
-use crate::data_packet::DataPacket;
-use crate::data_packet::DataEnum;
-use tokio_tungstenite::tungstenite::Error as TungsteniteError;
-use crate::data_packet::BestBidAskDataBTCBinance;
+use crate::data_packet::*;
+use crate::data_packet::SymbolEnum::*;
+use crate::data_packet::ExchangeEnum::*;
+use futures::task::{Context, Poll, noop_waker_ref};
+use std::pin::Pin;
+use futures_util::Stream;
+use flate2::read::GzDecoder;
+use tokio_tungstenite::{WebSocketStream, connect_async, MaybeTlsStream};
+use tokio_tungstenite::tungstenite::{Message, error::Error as TungsteniteError};
+use tokio::net::TcpStream;
+use futures_util::{StreamExt, SinkExt};
+use std::io::Read; // Needed for `read_to_end` method
+use serde_json::{Value, json}; // Importing `Value` and `json!` macro
+use tokio::runtime::Runtime;
 
 pub struct HuobiExchangeListener<'a> {
     id: i32,
@@ -14,7 +23,7 @@ pub struct HuobiExchangeListener<'a> {
 
 impl<'a> HuobiExchangeListener<'a> {
     pub fn new(id: i32, subscription: &'a mut WebSocket) -> Self {
-        HuobiExchangeListener { id, subscription }
+        HuobiExchangeListener { id, subscription, }
     }
 
     pub fn get_subscription(&mut self) -> &mut WebSocket {
@@ -36,45 +45,139 @@ impl<'a> ExchangeListener for HuobiExchangeListener<'a> {
 
     fn parse_message(&self, message: &str) -> Box<DataPacket> {
         let parsed_data: serde_json::Value = serde_json::from_str(message).expect("Unable to parse message");
-
-        let enum_creator = BestBidAskDataBTCHuobi {
-            bestask: parsed_data["tick"]["asks"][0][0].as_str().expect("Issue parsing JSON").parse().unwrap(),
-            askamt: parsed_data["tick"]["asks"][0][1].as_str().expect("Issue parsing JSON").parse().unwrap(),
-            bestbid: parsed_data["tick"]["bids"][0][0].as_str().expect("Issue parsing JSON").parse().unwrap(),
-            bidamt: parsed_data["tick"]["bids"][0][1].as_str().expect("Issue parsing JSON").parse().unwrap(),
+    
+        let enum_creator = MarketIncremental {
+            bestask: parsed_data["asks"][0][0].as_str().expect("Issue parsing JSON").parse().unwrap(),
+            askamount: parsed_data["asks"][0][1].as_str().expect("Issue parsing JSON").parse().unwrap(),
+            bestbid: 0.0,
+            bidamount: 0.0, //just for testing
         };
 
         let ret = DataPacket {
-            Data: DataEnum::BBAHuobiBTCData(enum_creator),
-            Exchange: String::from("Huobi"),
-            Channel: String::from("Channel 2"),
-            timestamp: parsed_data["ts"].as_str().expect("Issue parsing JSON").parse().unwrap()
+            data: DataEnum::MBP(enum_creator),
+            exchange: Huobi,
+            symbol_pair: BTCUSD,
+            channel: String::from("Channel 1"),
+            timestamp: 0,
         };
         Box::new(ret)
     }
+    fn poll(&mut self) -> Option<()> {
+        let waker = noop_waker_ref();
+        let mut context = Context::from_waker(&waker);
 
-    // No longer necessary
-    async fn on_message(&mut self, json: Option<&str>) {
-        if let Some(message) = json {
-            let _data_packet = self.parse_message(message);
-            // Maybe need to add more functionality with the parsed message
+        if let Some(socket) = self.get_subscription().get_mut_socket() {
+            let socket = Pin::new(socket);
+
+            match socket.poll_next(&mut context) {
+                Poll::Ready(Some(Ok(msg))) => {
+                    match msg {
+                        Message::Ping(ping_data) => {
+                            println!("Ping branch Reached");
+                            let pong_response = json!({ "pong": ping_data }).to_string();
+                            println!("Sent Pong response: {}", pong_response);
+                            self.subscription.send(&pong_response);
+                        },
+                        Message::Binary(data) => {
+                            println!("Binary branch reached!!!!");
+                            //println!("Received binary data: {:?}", data);
+            
+                            // Attempt to decompress the data using a GZIP decoder
+                            let mut decoder = GzDecoder::new(&data[..]);
+                            let mut decompressed_data = Vec::new();
+                            match decoder.read_to_end(&mut decompressed_data) {
+                                Ok(_) => {
+                                    println!("WE HAVE DECOMPRESSED THE DATA");
+                                    // println!("Decompressed data: {:?}", decompressed_data);
+                                    
+                                    // Convert decompressed data to text
+                                    let text = String::from_utf8(decompressed_data).expect("Found invalid UTF-8");
+                                    println!("Decompressed text: {}", text);
+            
+                                    // Respond to pings
+                                    if let Ok(parsed) = serde_json::from_str::<Value>(&text) {
+                                        if let Some(ping) = parsed.get("ping") {
+                                            let pong_response = json!({ "pong": ping }).to_string();
+                                            //self.subscription.send("").expect("Failed to send pong");
+                                            self.subscription.send(&pong_response);
+                                            println!("Sent Pong response: {}", pong_response);
+                                        }
+                                    }
+                                },
+                                Err(e) => {
+                                    println!("Failed to decompress GZIP data: {:?}", e);
+                                }
+                            }
+                        },
+                        Message::Text(text) => {
+                            println!("text branch reached");
+                            println!("Received text: {}", text);
+                            // Handle text message.
+                        },
+                        _ => {
+                            // Handle other message types
+                        }
+                    }
+
+                    Some(())
+                },
+                Poll::Ready(Some(Err(e))) => {
+                    println!("Error receiving message: {:?}", e);
+                    None
+                },
+                Poll::Ready(None) => {
+                    println!("Socket closed.");
+                    None
+                },
+                Poll::Pending => {
+                    println!("Waiting...");
+                    None
+                }
+            }
         } else {
-            println!("No message received");
+            println!("WebSocket is not connected.");
+            None
         }
     }
-
-    // No longer necessary
-    async fn next(&mut self) -> Option<Box<DataPacket>> {
+/*     
+        async fn next(&mut self) -> Option<Box<DataPacket>> {
         match self.subscription.receive().await {
-            Ok(Some(message)) => Some(self.parse_message(&message)),
-            Ok(None) => None,
+            Ok(Some(message)) => {
+                // We expect that the message is a JSON string here, whether it was originally text or binary.
+                if let Ok(parsed) = serde_json::from_str::<Value>(&message) {
+                    println!("parsed: {}", parsed);
+                    if parsed.get("ping").is_some() {
+                        // If it's a ping message, construct a pong response.
+                        println!("Received Ping: {:?}", parsed);
+                        let pong_response = json!({ "pong": parsed["ping"] }).to_string();
+                        // Send the pong response asynchronously and ignore errors here.
+                        let _ = self.subscription.send(&pong_response).await;
+                        println!("Sent Pong response: {}", pong_response);
+                        None
+                    } else {
+                        // Parse the message as a data packet if it's not a ping.
+                        println!("Received non-ping/pong message: {}", message);
+                        Some(self.parse_message(&message))
+                    }
+                } else {
+                    // If the message is not valid JSON, print an error message.
+                    println!("Received message that was not valid JSON: {}", message);
+                    None
+                }
+            },
+            Ok(None) => {
+                // No new messages are available at the moment.
+                None
+            },
             Err(e) => {
+                // An error occurred while trying to receive a message.
                 println!("Error receiving message: {:?}", e);
                 None
             }
         }
     }
-
+    */
+     
     fn set_id(&mut self, new_id: i32) {
         self.id = new_id;
     }
